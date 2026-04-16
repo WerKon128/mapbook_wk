ed0a121772dc73af13d9fe25a132e1173daf1dce
WerKon128 <weronika.konieczna01@student.wat.edu.pl>
2026-04-16 14:00:49 +0200

    wip: mvc template applied

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-users: list = [
-    {'username': 'Oliwia', 'location': 'Łódź', 'posts': 1,
-     'usermessage': ['życzenia1', 'kocham legie1', 'sprzedam opla1', 'kiwi1']},
-    {'username': 'Paweł', 'location': 'Ostróda', 'posts': 1,
-     'usermessage': ['życzenia2', 'kocham legie2', 'sprzedam opla2']},
-    {'username': 'Elizka', 'location': 'Łódź', 'posts': 1, 'usermessage': ['życzenia3', 'kocham legie3']},
-    {'username': 'Filip', 'location': 'Dęblin', 'posts': 1,
-     'usermessage': ['życzenia4', 'kocham legie4', 'sprzedam opla4', 'kiwi4']},
-]
+from mapbook_lib.model import users
+from mapbook_lib.controller import read_data
 
 
-def read_data(users_data: list)-> None:
-
-    for user in users_data:
-         print(
-             f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
 
 
 read_data(users[1:])
