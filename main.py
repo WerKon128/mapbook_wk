0f6bf5aa2b45d75e3a3ca3df443d003c2144bdbd
WerKon128 <weronika.konieczna01@student.wat.edu.pl>
2026-04-16 13:26:45 +0200

    wip: complex data structure

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 users: list = [
     {'username': 'Oliwia', 'location':'Łódź','posts':1, 'usermessage':['życzenia1', 'kocham legie1', 'sprzedam opla1', 'kiwi1']} ,
-    {'username': 'Paweł', 'location':'Ostróda','posts':1, 'usermessage':['życzenia2', 'kocham legie2', 'sprzedam opla2', 'kiwi2']} ,
+    {'username': 'Paweł', 'location':'Ostróda','posts':1, 'usermessage':['życzenia2', 'kocham legie2', 'sprzedam opla2']} ,
     {'username': 'Elizka', 'location':'Łódź','posts':1, 'usermessage':['życzenia3', 'kocham legie3']},
     {'username': 'Filip', 'location':'Dęblin','posts':1, 'usermessage':['życzenia4', 'kocham legie4', 'sprzedam opla4', 'kiwi4']},
 ]
