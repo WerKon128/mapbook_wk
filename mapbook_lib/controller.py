409c1adbedfb44f67aaa66218064ccd9685e29ea
WerKon128 <weronika.konieczna01@student.wat.edu.pl>
2026-04-30 13:42:25 +0200

    feat: add update function

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -1,8 +1,7 @@
-
-def read_data(users_data: list)-> None:
+def read_data(users_data: list) -> None:
     for user in users_data:
-         print(
-             f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
+        print(
+            f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
 
 
 def add_user(users_data: list) -> None:
@@ -12,3 +11,21 @@ def add_user(users_data: list) -> None:
     usermessage = ['']
     users_data.append({'username': name, 'location': location, 'posts': posts,
                        'usermessage': usermessage}, )
+
+
+def remove_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do usunięcia: ')
+
+    for user in users_data:
+        if user['username'] == name:
+            users_data.remove(user)
+
+
+def update_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do zmiany: ')
+
+    for user in users_data:
+        if user['username'] == name:
+            user['username'] = input('Podaj nowe imie: ')
+            user['location'] = input('Podaj nową lokalizację: ')
+            user['posts'] = int(input('Podaj liczbę postów'))
